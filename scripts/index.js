e3f3d0bc8ab044d8ce01a7032eff172afa57aad4
excalibur-kvrv <36708857+excalibur-kvrv@users.noreply.github.com>
2018-11-11 23:30:27 +0530

    update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,21 @@
 function playSound(e) {
        const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
        const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-       if(!audio)
+       if(!audio) {
               return;
+       }       
        audio.currentTime = 0;
        audio.play();
-       key.classList.add('playing');
+       key.classList.add("playing");
 }
 
 function removeTransition(e) {
-       if(e.propertyName!=='transform')
+       if(e.propertyName!=="transform") {
               return;
-       this.classList.remove('playing');
+       }       
+       this.classList.remove("playing");
 }
 
-const keys = document.querySelectorAll('.key');
-keys.forEach(key => key.addEventListener('transitionend',removeTransition));
-window.addEventListener('keydown',playSound);
+const keys = document.querySelectorAll(".key");
+keys.forEach(key => key.addEventListener("transitionend",removeTransition));
+window.addEventListener("keydown",playSound);
